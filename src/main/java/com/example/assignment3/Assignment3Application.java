9ec4db5341f54a5e60f29ce8b5a56834e24b7161
Ole Syverinsen <ole.nikolai.ravn.syverinsen@no.experis.com>
2022-08-29 09:36:09 +0200

    Commiting project

--- src/main/java/com/example/assignment3/Assignment3Application.java
+++ src/main/java/com/example/assignment3/Assignment3Application.java
@@ -8,6 +8,7 @@ public class Assignment3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Assignment3Application.class, args);
+        //Something here
     }
 
 }
