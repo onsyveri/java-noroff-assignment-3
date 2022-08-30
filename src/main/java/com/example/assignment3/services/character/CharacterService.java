737aed56c8588c28ac9a33dd595056b41de62c51
Carl Markus Malde <carl.markus.malde@no.experis.com>
2022-08-30 10:30:14 +0200

    started on character repository/services

---
+++ src/main/java/com/example/assignment3/services/character/CharacterService.java
@@ -0,0 +1,8 @@
+package com.example.assignment3.services.character;
+
+import com.example.assignment3.services.CrudService;
+
+public interface CharacterService extends CrudService<Character, Integer> {
+
+
+}
---
+++ src/main/java/com/example/assignment3/services/character/CharacterServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.assignment3.services.character;
+
+import com.example.assignment3.repositories.CharacterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class CharacterServiceImpl implements CharacterService{
+    private final CharacterRepository characterRepository;
+
+    public CharacterServiceImpl(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
+    }
+
+
+    @Override
+    public Character findById(Integer id) {
+        //return characterRepository.findById(id).orElse(null);
+        return null;
+
+    }
+
+    @Override
+    public Collection<Character> findAll() {
+        return null;
+    }
+
+    @Override
+    public Character add(Character entity) {
+        return null;
+    }
+
+    @Override
+    public Character update(Character entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(Character entity) {
+
+    }
+}
